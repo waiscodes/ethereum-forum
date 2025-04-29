1981b839039c5ede37f239e95d587a961186beb5
Luc <luc@lucemans.nl>
2025-04-29 08:42:56 +0200

    Update slicing

--- web/src/components/topic/TopicsTrending.tsx
+++ web/src/components/topic/TopicsTrending.tsx
@@ -14,7 +14,7 @@ export const TopicsTrending: FC = () => {
         <div className="space-y-4">
             <div className="text-lg font-bold border-b border-b-primary">Trending this week</div>
             <div className="grid gap-2 grid-cols-1 md:grid-cols-2 mx-auto">
-                {data?.sort((a, b) => new Date(b.last_post_at ?? '').getTime() - new Date(a.last_post_at ?? '').getTime()).slice(0, 6).map((topic) => <TopicPreview key={topic.topic_id} topic={topic} />)}
+                {data?.slice(0, 6).sort((a, b) => new Date(b.last_post_at ?? '').getTime() - new Date(a.last_post_at ?? '').getTime()).map((topic) => <TopicPreview key={topic.topic_id} topic={topic} />)}
             </div>
         </div>
     );
