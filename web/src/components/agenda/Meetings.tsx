eccec7b7cc24e70791406685b88d74900dc7cecb
Luc <luc@lucemans.nl>
2025-05-06 20:28:34 +0200

    Update wallet list

--- web/src/components/agenda/Meetings.tsx
+++ web/src/components/agenda/Meetings.tsx
@@ -1,5 +1,5 @@
 import { Link } from '@tanstack/react-router';
-import { parseISO } from 'date-fns';
+import { format, parseISO } from 'date-fns';
 import { FC } from 'react';
 import { FiRefreshCcw } from 'react-icons/fi';
 import { SiGooglemeet, SiYoutube, SiZoom } from 'react-icons/si';
@@ -11,11 +11,13 @@ import { TimeAgo } from '../TimeAgo';
 
 export const Meetings: FC<{ data: CalendarEvent[] }> = ({ data }) => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-1 gap-3">
-            {data?.map((event) => (
-                // @ts-ignore
-                <MeetingPreview key={event.uid + event.start?.toString()} event={event} />
-            ))}
+        <div className="flex gap-3">
+            <div className="flex flex-col">
+                {data?.map((event) => (
+                    // @ts-ignore
+                    <MeetingPreview key={event.uid + event.start?.toString()} event={event} />
+                ))}
+            </div>
         </div>
     );
 };
@@ -52,49 +54,59 @@ export const MeetingPreview = ({ event }: { event: CalendarEvent }) => {
     const occurence = getOccurence(event.pm_data, event.pm_number);
 
     return (
-        <div className="card flex gap-2">
-            {occurence &&
-                'youtube_streams' in occurence &&
-                occurence.youtube_streams?.[0]?.stream_url && (
-                    <div className="w-full aspect-video max-w-40 flex items-center">
-                        <img
-                            src={convertYoutubeUrlToThumbnailUrl(
-                                occurence.youtube_streams?.[0].stream_url
-                            )}
-                            alt="Youtube Stream"
-                            className="w-full aspect-video"
-                        />
-                    </div>
-                )}
-            <div className="flex flex-col gap-2 grow">
-                <div className="flex items-center gap-2 justify-between">
-                    <h3 className="font-bold">{event.summary}</h3>
-                    <div className="flex items-center gap-2">
-                        {event.pm_number && (
-                            <Link
-                                to={'/pm/$issueId'}
-                                params={{ issueId: event.pm_number.toString() }}
-                                className="button"
-                            >
-                                #{event.pm_number}
-                            </Link>
-                        )}
-                        {event.occurance == 'Recurring' && <FiRefreshCcw className="size-3" />}
-                    </div>
+        <div className="flex gap-2">
+            <div className="border-r w-full border-primary pr-2 max-w-24 break-all text-right">
+                <div className="text-sm">
+                    {event?.start && format(parseISO(event?.start), 'HH:mm')}
                 </div>
-                <div
-                    dangerouslySetInnerHTML={{ __html: event.description ?? '' }}
-                    className="grow max-h-40 overflow-y-auto"
-                />
-                <div className="flex justify-between flex-wrap">
-                    <div className="flex flex-wrap gap-2">
-                        {event.meetings.map((meeting) => (
-                            <MeetingLink key={meeting.link} meeting={meeting} />
-                        ))}
+                <div className="text-xs">
+                    (<TimeAgo date={parseISO(event.start)} />)
+                </div>
+            </div>
+            <div className="card flex gap-2 grow mb-4">
+                {occurence &&
+                    'youtube_streams' in occurence &&
+                    occurence.youtube_streams?.[0]?.stream_url && (
+                        <div className="w-full aspect-video max-w-40 flex items-center">
+                            <img
+                                src={convertYoutubeUrlToThumbnailUrl(
+                                    occurence.youtube_streams?.[0].stream_url
+                                )}
+                                alt="Youtube Stream"
+                                className="w-full aspect-video"
+                            />
+                        </div>
+                    )}
+                <div className="flex flex-col gap-2 grow">
+                    <div className="flex items-center gap-2 justify-between">
+                        <h3 className="font-bold">{event.summary}</h3>
+                        <div className="flex items-center gap-2">
+                            {event.pm_number && (
+                                <Link
+                                    to={'/pm/$issueId'}
+                                    params={{ issueId: event.pm_number.toString() }}
+                                    className="button"
+                                >
+                                    #{event.pm_number}
+                                </Link>
+                            )}
+                            {event.occurance == 'Recurring' && <FiRefreshCcw className="size-3" />}
+                        </div>
                     </div>
-                    <p className="text-sm text-gray-500 text-end flex items-end">
-                        {event.start && <TimeAgo date={parseISO(event.start)} />}
-                    </p>
+                    <div
+                        dangerouslySetInnerHTML={{ __html: event.description ?? '' }}
+                        className="grow max-h-40 overflow-y-auto"
+                    />
+                    <div className="flex justify-between flex-wrap">
+                        <div className="flex flex-wrap gap-2">
+                            {event.meetings.map((meeting) => (
+                                <MeetingLink key={meeting.link} meeting={meeting} />
+                            ))}
+                        </div>
+                        <p className="text-sm text-gray-500 text-end flex items-end">
+                            {event.start && <TimeAgo date={parseISO(event.start)} />}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
