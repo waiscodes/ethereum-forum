3e89a15d5332d2f24af943375c1bd6bc555da138
Luc <luc@lucemans.nl>
2025-06-03 14:33:44 +0200

    Improve overflow

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -13,7 +13,7 @@ function RootComponent() {
     return (
         <AppWrapper>
             <Navbar />
-            <div className="flex flex-col gap-1 pb-16">
+            <div className="flex flex-col gap-1 pb-16 max-w-screen">
                 <Sidebar />
                 <Outlet />
             </div>
