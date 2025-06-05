3fba2df31a0c1a63d244f3815a15368d27f2d972
Luc <luc@lucemans.nl>
2025-06-05 17:48:17 +0200

    Update workshop

--- web/src/routes/chat/$chatId.tsx
+++ web/src/routes/chat/$chatId.tsx
@@ -11,6 +11,10 @@ function RouteComponent() {
     return (
         <div className="mx-auto w-full max-w-screen-lg pt-8 px-2 space-y-4">
             <div>Welcome to the workshop!</div>
+            <div className="card space-y-2">
+                <p>The workshop is currently in beta and not publicly accessible yet.</p>
+                <p>Please check back later.</p>
+            </div>
         </div>
     );
 }
